e5ccdb74bae9504315abf1051639da9debebc6dd
jasper <whiteana001@warren.k12.in.us>
2024-11-19 12:08:43 -0500

    added text box

--- app/src/main/java/com/example/tasksapp/MainActivity.kt
+++ app/src/main/java/com/example/tasksapp/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.example.tasksapp
 
 import android.os.Bundle
+import android.widget.NumberPicker.OnValueChangeListener
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.tasksapp.ui.theme.TasksAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +37,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TasksAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    TaskApp(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -31,17 +48,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun TaskApp(name: String, modifier: Modifier = Modifier) {
+    Column (
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start,
+        modifier = modifier
+            .height(500.dp)
+            .width(300.dp)
+    ){
+        Text(
+            text = stringResource(R.string.to_do_list),
+            modifier = Modifier
+                .padding(bottom = 16.dp, top = 40.dp)
+                .align(alignment = Alignment.Start)
+        )
+
+
+        AddTask(modifier = Modifier
+            .padding(bottom = 24.dp)
+            .fillMaxWidth())
+    }
+}
+
+@Composable
+fun AddTask(modifier: Modifier = Modifier){
+    var newTask by remember { mutableStateOf("") }
+    TextField(
+        value = newTask,
+        onValueChange = { newTask = it},
+        label = { Text(stringResource(R.string.input_text))},
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(),
         modifier = modifier
     )
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TasksAppTheme {
-        Greeting("Android")
+        TaskApp("Android")
     }
 }
